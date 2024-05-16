4e41324dda27a16832842ffca464b0abccc2278b
ian3v <3ian9v@gmail.com>
2024-05-16 12:25:11 -0500

    subiendo todo en orden 6.1.1

--- projects/05-react-buscador-peliculas/src/Hooks/useMOvies.js
+++ projects/05-react-buscador-peliculas/src/Hooks/useMOvies.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import withResults from '../Mocks/with-result.json' // responseMovies es una platilla para los resultados sin conexion
 import NotResult from '../Mocks/not-result.json'
-// import { searchMovies } from "../Services/SearchMovies"
+import { searchMovies } from "../Services/SearchMovies"
 
 export function useMovies (search) {
  
@@ -21,18 +21,15 @@ export function useMovies (search) {
 
   const getMovie = (search)=>{  
 
-    // const resultSearch = searchMovies({search})
 
-    // console.log('%c26 >','color:red;font-size:15px;',resultSearch);
 
     if(search){ 
       const link = `https://www.omdbapi.com/?apikey=e9237489&s=${search}`
-      console.log('%c24 >','color:yellow;font-size:15px;',`https://www.omdbapi.com/?apikey=e9237489&s=${search}
-      `); 
+  
       fetch(link)
         .then(res => res.json())
         .then(data => {
-          console.log('%c30 >','color:yellow;font-size:15px;',data.Search);
+          console.log('%c35 useMovies >','color:yellow;font-size:15px;',data.Search);
           setResultMovies(data)
         })
 
--- projects/05-react-buscador-peliculas/src/Services/SearchMovies.js
+++ projects/05-react-buscador-peliculas/src/Services/SearchMovies.js
@@ -2,36 +2,36 @@ import { useState } from "react";
 
 const API_KEY = 'e9237489'
 
-export const searchMovies = ( {search})=>{
+export const searchMovies = async ( {search})=>{
     // const [resultMovie, setResultMovies] = useState([])
 
 
     if(search){
         console.log('%c7 serachMOvies >','color:;font-size:15px;',search);
 
-        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
+        return fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
             .then(res => res.json())
             .then( data => {
                 console.log('%c12 >','color:yellow;font-size:15px;',data.Search);
 
                 // setResultMovies(data.Search)
-                const movie = resultMovie.Search
+                const movie = data.Search
 
-                const mappedMovies = movie?.map( ()=>(
+                movie?.map( ()=>(
                     {
                         id: element.imdbID,
                         title: element.Title,
                         year: element.Year,
                         poster: element.Poster,
                     }
+
                 ) )
-            
-                return mappedMovies
+                console.log('%c29 >','color:violet;font-size:15px;',movie);
               })
     }else{
         return  'no hola'
     }
 
-    console.log('%c34 >','color:red;font-size:15px;',mappedMovies);
+    // console.log('%c34 >','color:red;font-size:15px;',mappedMovies);
 }
 
